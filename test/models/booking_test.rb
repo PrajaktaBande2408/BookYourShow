b77d9680ef0a18da5babe233c4dd09d5202feb95
PrajaktaBande2408 <prajaktabande1711@gmail.com>
2024-03-19 15:19:42 -0700

    fixed test cases

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should belong to a showtime" do
+    booking = bookings(:one)
+    assert_instance_of Showtime, booking.showtime
+  end
 end
